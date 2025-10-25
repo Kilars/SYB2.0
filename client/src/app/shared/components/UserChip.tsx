4b3a6d87c1667ae14bc7357e63ae4f0dadcec11b
Lars Skifjeld <lars.skifjeld@bouvet.no>
2025-10-25 13:27:56 +0200

    color hash username for profile icon

--- client/src/app/shared/components/UserChip.tsx
+++ client/src/app/shared/components/UserChip.tsx
@@ -1,18 +1,26 @@
 import { Person } from "@mui/icons-material";
 import { Chip } from "@mui/material";
 import { Link } from "react-router";
+import ColorHash from "color-hash";
 
 type Props = {
     userId: string;
     displayName: string;
 }
 export default function UserChip({ userId, displayName }: Props) {
+    const colorHash = new ColorHash();
     return (
         <Chip
             icon={<Person />}
             label={displayName}
             color={'default'}
-            sx={{ display: 'flex', justifyContent: 'flex-start', flexGrow: 1, minWidth: 0 }}
+            sx={{
+                display: 'flex',
+                justifyContent: 'flex-start',
+                flexGrow: 1,
+                minWidth: 0,
+                '& .MuiChip-icon': { color: colorHash.hex(displayName) }
+            }}
 
             clickable
             component={Link}
